e08c6c00a1e2df984faea28e4802d44fe01190b0
Alex Benoit <alexander.benoit@outlook.com>
2016-08-17 15:14:25 +0100

    done validation and association of restaurant and reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817121618) do
+ActiveRecord::Schema.define(version: 20160817140351) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -24,12 +24,10 @@ ActiveRecord::Schema.define(version: 20160817121618) do
   create_table "reviews", force: :cascade do |t|
     t.text     "content"
     t.integer  "rating"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "restaurant_id_id"
     t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
-    t.index ["restaurant_id_id"], name: "index_reviews_on_restaurant_id_id"
   end
 
 end
